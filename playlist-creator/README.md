b8d4b0ea3fe972d35991d94acca96c89a9973515
Julian Sanders-Kay <sanderskay@hotmail.com>
2021-01-25 15:08:17 -0500

    Clean up files and add initial CSS

--- playlist-creator/README.md
+++ playlist-creator/README.md
@@ -1,3 +1,5 @@
 # Music-Playlist-Creator
 
-Website that will allow you to search and pull songs from Spotify and add them to a custom playlist on this site. It will use React.
+Website that will allow you to search and pull songs from Spotify API and add them to a custom playlist on this site.
+
+It will use React.
---
+++ playlist-creator/src/Components/App/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import Playlist from './Playlist';
+import SearchBar from './SearchBar';
+import SearchResults from './SearchResults';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App">
+      <!-- Add a SearchBar component -->
+       <div className="App-playlist">
+      <!-- Add a SearchResults component -->
+      <!-- Add a Playlist component -->
+      </div>
+       </div>
+    </div>
+  );
+}
+
+export default App;
