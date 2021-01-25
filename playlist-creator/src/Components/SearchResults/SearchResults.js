1630055d1928cc046e99fe6b38f628aaf4aacfcd
Julian Sanders-Kay <sanderskay@hotmail.com>
2021-01-25 15:31:54 -0500

    Initial Search Components

---
+++ playlist-creator/src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SearchResults.css';
+import './TrackList';
+
+function SearchResults(){
+    return(
+        <div className="SearchResults">
+          <h2>Results</h2>
+         <!-- Add a TrackList component -->
+        </div>
+    )
+}
+
+export default SearchResults;
