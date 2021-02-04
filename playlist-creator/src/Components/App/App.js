3b089e1a5051cd333d137e6031de933bf20c8ed8
Julian Sanders-Kay <sanderskay@hotmail.com>
2021-02-04 15:11:00 -0500

    Complete Components

--- playlist-creator/src/Components/App/App.js
+++ playlist-creator/src/Components/App/App.js
@@ -1,42 +1,26 @@
+import React from "react";
 import "./App.css";
-import Playlist from "../Playlist/Playlist";
+
 import SearchBar from "../SearchBar/SearchBar";
 import SearchResults from "../SearchResults/SearchResults";
-import React from "react";
+import Playlist from "../Playlist/Playlist";
+
+import Spotify from "../../util/Spotify";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      SearchResults: [
-        { name: "name1", artist: "artist1", album: "album1", id: 1 },
-        { name: "name2", artist: "artist2", album: "album2", id: 2 },
-        { name: "name3", artist: "artist3", album: "album3", id: 3 },
-      ],
+      searchResults: [],
       playlistName: "My Playlist",
-      playlistTracks: [
-        {
-          name: "playlistName1",
-          artist: "playlistArtist1",
-          album: "playlistAlbum1",
-          id: 4,
-        },
-        {
-          name: "playlistName2",
-          artist: "playlistArtist2",
-          album: "playlistAlbum2",
-          id: 5,
-        },
-        {
-          name: "playlistName3",
-          artist: "playlistArtist3",
-          album: "playlistAlbum3",
-          id: 6,
-        },
-      ],
+      playlistTracks: [],
     };
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
 
   addTrack(track) {
@@ -44,26 +28,58 @@ class App extends React.Component {
     if (tracks.find((savedTrack) => savedTrack.id === track.id)) {
       return;
     }
+
     tracks.push(track);
     this.setState({ playlistTracks: tracks });
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter((currentTrack) => currentTrack.id !== track.id);
+
+    this.setState({ playlistTracks: tracks });
+  }
+
+  updatePlaylistName(name) {
+    this.setState({ playlistName: name });
+  }
+
+  savePlaylist() {
+    const trackUris = this.state.playlistTracks.map((track) => track.uri);
+    Spotify.savePlaylist(this.state.playlistName, trackUris).then(() => {
+      this.setState({
+        playlistName: "New Playlist",
+        playlistTracks: [],
+      });
+    });
+  }
+
+  search(term) {
+    console.log(term);
+    Spotify.search(term).then((searchResults) => {
+      this.setState({ searchResults: searchResults });
+    });
   }
 
   render() {
     return (
       <div>
         <h1>
-          Play<span class="highlight">LIST</span>ing
+          Ja<span className="highlight">mmm</span>ing
         </h1>
-        <div class="App">
-          <SearchBar />
-          <div class="App-playlist">
+        <div className="App">
+          <SearchBar onSearch={this.search} />
+          <div className="App-playlist">
             <SearchResults
-              SearchResults={this.state.searchResults}
+              searchResults={this.state.searchResults}
               onAdd={this.addTrack}
             />
             <Playlist
               playlistName={this.state.playlistName}
               playlistTracks={this.state.playlistTracks}
+              onRemove={this.removeTrack}
+              onNameChange={this.updatePlaylistName}
+              onSave={this.savePlaylist}
             />
           </div>
         </div>
--- playlist-creator/src/Components/TrackList/TrackList.js
+++ playlist-creator/src/Components/TrackList/TrackList.js
@@ -1,55 +1,27 @@
 import React from "react";
-import Track from "../Track/Track";
 import "./TrackList.css";
+import Track from "../Track/Track";
 
 class TrackList extends React.Component {
-    renderMap(){
-        if (this.props.tracks){
-           
-         return  this.props.tracks.map(track => {return <Track track={track}  key={track.id} onAdd={this.props.onAdd}/> });
-           
-        } 
-       }
-       
-       render() {
-       
-           return (
-               
-               <div className="TrackList">
-               {this.renderMap()}
-               </div>
-           
-           )
-               
-       }
+  renderMap() {
+    if (this.props.tracks) {
+      return this.props.tracks.map((track) => {
+        return (
+          <Track
+            track={track}
+            key={track.id}
+            onAdd={this.props.onAdd}
+            onRemove={this.props.onRemove}
+            isRemoval={this.props.isRemoval}
+          />
+        );
+      });
+    }
+  }
+
+  render() {
+    return <div className="TrackList">{this.renderMap()}</div>;
+  }
 }
 
 export default TrackList;
-
-
-
-{/*class TrackList extends React.Component {
-  render() {
-    if (this.props.tracks) {
-      var myList = this.props.tracks.map((track) => {
-        return <Track track={track} key={track.id} />;
-      });
-    }
-
-    return <div className="Tracklist">{myList}</div>;
-  }
-} */}
-
-{/*
-class TrackList extends React.Component {
-  render() {
-    return (
-      <div className="Tracklist">
-        {this.props.tracks.map(track => {
-          return <Track track={track} key={track.id} />;
-        })}
-      </div>
-    );
-  }
-}
- */}
